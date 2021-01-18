776a5fac6caf322322a7c8ea02cd223dafc4ae41
Kresna <satyakresna6295@gmail.com>
2021-01-18 19:13:16 +0800

    simplify configuration deploy to vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,13 +25,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if (!app()->isLocal()) {
             \URL::forceScheme('https');
-
-            $view = config('view.compiled');
-            foreach ([$view] as $path) {
-                if (! is_dir($path)) {
-                    mkdir($path, 0755, true);
-                }
-            }
         }
     }
 }
