23b17f1a0af81d947537025bfb516142dddc84c2
Kresna <satyakresna6295@gmail.com>
2021-01-18 19:31:55 +0800

    set view compile path in vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
     {
         if (!app()->isLocal()) {
             \URL::forceScheme('https');
+
+            $view = config('view.compiled');
+            foreach ([$view] as $path) {
+                if (! is_dir($path)) {
+                    mkdir($path, 0755, true);
+                }
+            }
         }
     }
 }
